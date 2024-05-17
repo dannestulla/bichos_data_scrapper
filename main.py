86b061efdbfe7f3da3277f3c00cc1a277634480d
DannEstulla <dannestulla@gmail.com>
2024-05-17 10:16:09 -0300

    feat: files downloaded were not written on .csv, fixed

--- main.py
+++ main.py
@@ -4,66 +4,106 @@ import subprocess
 import boto3
 from botocore.config import Config
 from credentials import bucket_name, r2_endpoint, access_key, secret_key
-from pictures_model import PostImages, post_pictures_serializer, Images
 from botocore.exceptions import NoCredentialsError
+
+from downloaded_files_csv import read_csv_file, write_files_downloaded_to_csv
 
 
 def main():
-    pages = ['meubichotasalvocanoas']
+    pages = [
+        'meubichotasalvocanoas',
+        'acheseupetrs',
+        'acheseudogulbra'
+        'petresgatado_canoas',
+        'animaisresgatadosmathias',
+        'caesresgatadoscanoas',
+        'onlycats.canoas'
+    ]
     #run_instaloader(pages)
-    folder_path = 'C:\\Users\\dannn\\IdeaProjects\\BichosResgate\\bichos_data_scrapper\\' + pages[0]
-    images_paths = get_images_path(pages, folder_path)
-    upload_to_s3(images_paths, pages)
+    folder_paths = create_folder_paths(pages)
+    check_if_folders_exist(folder_paths)
+    s3_client = create_boto_client()
+    index = 0
+    for page in pages:
+        page_file_paths = get_files_paths(folder_paths[index])
+        files_already_downloaded = read_csv_file(page, page_file_paths)
+        files_just_downloaded = check_files_just_downloaded(page_file_paths)
+        files_not_uploaded = [item for item in files_just_downloaded if item not in files_already_downloaded]
+        upload_to_s3(files_not_uploaded, page, s3_client)
+        write_files_downloaded_to_csv(files_not_uploaded, page)
+        upload_file_list_to_s3(page, s3_client)
+        index += 1
 
 
 def run_instaloader(pages):
     # fast-update just downloads new pics
-    comando = 'instaloader --fast-update --filename-pattern="{date_utc:%Y-%m-%d}" ' + pages[0]
+    for page in pages:
+        comando = 'instaloader --fast-update --post-filter="date_utc >= datetime(2024, 5, 10)" ' + page
+        current_dir = os.getcwd()
+        parent_dir = os.path.dirname(current_dir)
+        run_instaloader_result = subprocess.run(comando, shell=True, text=True, cwd=parent_dir)
+        if run_instaloader_result.returncode == 0:
+            print("run_instaloader executado com sucesso!")
+        else:
+            print("Erro ao executar comando!")
 
-    current_dir = os.getcwd()
-    parent_dir = os.path.dirname(current_dir)
-    run_instaloader_result = subprocess.run(comando, shell=True, text=True, cwd=parent_dir)
-
-    if run_instaloader_result.returncode == 0:
-        print("Comando executado com sucesso!")
-    else:
-        print("Erro ao executar comando!")
-
-
-def get_images_path(pages, path):
-    images_path = []
-    for file in os.listdir(path):
-        if file.endswith(('.jpeg', '.jpg', '.png', '.gif', '.bmp')):
-            full_path = os.path.join(path, file)
-            images_path.append(full_path)
-
-    return images_path
+def check_files_just_downloaded(files_paths):
+    file_names = []
+    for file_name in files_paths:
+        file_names.append(file_name.split("\\")[-2]+"/"+file_name.split("\\")[-1])
+    return file_names
 
 
-def upload_to_s3(list_images, pages):
-    count = 0
-    s3_client = boto3.client('s3',
-                             endpoint_url=r2_endpoint,
-                             aws_access_key_id=access_key,
-                             aws_secret_access_key=secret_key,
-                             config=Config(signature_version='s3v4'),
-                             region_name='auto'
-                             )
-    while count < 1:
-        file_name = list_images[count].split('\\')[7]
-        s3_file_name = pages[0] + '/' + file_name
+def create_folder_paths(pages):
+    folderPaths = []
+    for page in pages:
+        folderPaths.append('C:\\Users\\dannn\\IdeaProjects\\BichosResgate\\bichos_data_scrapper\\' + page)
+    return folderPaths
 
+
+def get_files_paths(folder_path):
+    images_paths = []
+    for file in os.listdir(folder_path):
+        if file.endswith(('.jpeg', '.jpg', '.png', '.gif', '.bmp', '.txt')):
+            full_path = os.path.join(folder_path, file)
+            images_paths.append(full_path)
+    print("Caminho das imagens criados")
+    return images_paths
+
+
+def create_boto_client():
+    return boto3.client('s3',
+                        endpoint_url=r2_endpoint,
+                        aws_access_key_id=access_key,
+                        aws_secret_access_key=secret_key,
+                        config=Config(signature_version='s3v4'),
+                        region_name='auto'
+                        )
+
+
+def upload_to_s3(list_images, page, s3_client):
+    for image_file_name in list_images:
+        file_name = image_file_name.split('/')[-1]
+        file_path2 = 'C:\\Users\\dannn\\IdeaProjects\\BichosResgate\\bichos_data_scrapper\\' + page + '\\' + file_name
         try:
-            s3_client.upload_file(list_images[count], bucket_name, s3_file_name)
+            print("Uploading " + image_file_name)
+            s3_client.upload_file(file_path2, bucket_name, image_file_name)
         except NoCredentialsError:
             print("Credenciais não disponíveis")
             return False
-        count = count + 1
-    prefix = '/gohan-bichos-resgate/'+pages[0]+'/'
-    response = s3_client.list_objects_v2(Bucket='bichos-central', Prefix='gohan-bichos-resgate/')
-    with open('files_names.txt', 'w') as files_names:
-        files_names.write(response)
-        s3_client.upload_file(files_names, 'bichos-central', 'files_names.txt')
+
+
+def check_if_folders_exist(folder_paths):
+    for folder_path in folder_paths:
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+
+def upload_file_list_to_s3(page, s3_client):
+    file_list_location = "C:\\Users\\dannn\\IdeaProjects\\BichosResgate\\bichos_data_scrapper\\bichos_data_scrapper\\" + page + ".csv"
+    s3_client.upload_file(file_list_location, bucket_name, page + ".csv")
+    print("Enviada lista de arquivos baixados")
+
 
 if __name__ == "__main__":
     main()
